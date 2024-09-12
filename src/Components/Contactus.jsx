2e196510c856d13b2281f9f4569b432f623181e7
premkuamr <kumarprem75715@gmail.com>
2024-09-12 11:08:23 +0530

    frontend pushed

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import axios from 'axios'
+import axios from "axios";
 const Contactus = () => {
   const [data, setdata] = useState({
-    name:"",email:"",message:""
-  })
+    name: "",
+    email: "",
+    message: "",
+  });
   const [issue, setissue] = useState({
-    name:"",email:"",password:""
-  })
+    name: "",
+    email: "",
+    password: "",
+  });
   const email_regx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   let valid = true;
- async  function validate(e) {
+  async function validate(e) {
     e.preventDefault();
     if (data.name === "") {
       valid = false;
       setissue({ name: "please provide valid name" });
-    }
-    else if (!email_regx.test(data.email)) {
+    } else if (!email_regx.test(data.email)) {
       valid = false;
-      setissue({ email:"please provide valid email addreess"})
-    }
-    else if (data.message === "") {
+      setissue({ email: "please provide valid email addreess" });
+    } else if (data.message === "") {
       valid = false;
       setissue({ message: "please give your feedback" });
-    }
-    else if (valid) {
-      setissue({ name: "", email: "", message: "" })
+    } else if (valid) {
+      setissue({ name: "", email: "", message: "" });
       alert(`${data.name} Thank you providing feedback 🎉`);
-      await axios.post("http://localhost:3600/feedback", data);
-      setdata({name:"",email:"",message:""})
-   }
+      await axios.post("https://backend-portfolio-peach.vercel.app/", data);
+      setdata({ name: "", email: "", message: "" });
     }
-  
+  }
+
   return (
     <div className="container">
       <Header />
@@ -61,7 +62,14 @@ const Contactus = () => {
                 placeholder="Enter your Name"
                 onChange={(e) => setdata({ ...data, name: e.target.value })}
               />
-              <p style={{ color: "red",fontSize:"12px", fontFamily: "sans-serif",position:"fixed" }}>
+              <p
+                style={{
+                  color: "red",
+                  fontSize: "12px",
+                  fontFamily: "sans-serif",
+                  position: "fixed",
+                }}
+              >
                 {issue.name}
               </p>
               <label htmlFor="email" className="col-md-12 mt-3">
@@ -75,7 +83,14 @@ const Contactus = () => {
                 placeholder="Enter your Email"
                 onChange={(e) => setdata({ ...data, email: e.target.value })}
               />
-              <p style={{ color: "red", fontFamily: "sans-serif",fontSize:"12px",position:"fixed" }}>
+              <p
+                style={{
+                  color: "red",
+                  fontFamily: "sans-serif",
+                  fontSize: "12px",
+                  position: "fixed",
+                }}
+              >
                 {issue.email}
               </p>
               <label htmlFor="message" className="col-md-12 mt-3">
@@ -89,10 +104,19 @@ const Contactus = () => {
                 placeholder="Message Here"
                 onChange={(e) => setdata({ ...data, message: e.target.value })}
               ></textarea>
-              <p style={{ color: "red", fontFamily: "sans-serif",fontSize:"12px",position:"fixed" }}>
+              <p
+                style={{
+                  color: "red",
+                  fontFamily: "sans-serif",
+                  fontSize: "12px",
+                  position: "fixed",
+                }}
+              >
                 {issue.message}
               </p>
-              <button className="offset-md-3 col-md-3 mt-4 text-center">Send</button>
+              <button className="offset-md-3 col-md-3 mt-4 text-center">
+                Send
+              </button>
             </form>
           </div>
         </div>
